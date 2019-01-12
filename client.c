527d7ef4d25e36ebd56229c87bce4d91200f13b2
MadhavChoudhary <madhavtummala@icloud.com>
2019-01-13 01:54:48 +0530

    Option for client to leave

--- client.c
+++ client.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>       		//EXIT_FAILURE, memset
 #include <string.h>				//strlen()
 #include <netinet/in.h>			//for struct sockaddr
-#include <pthread.h>
-
+#include <pthread.h>			//for multi threading
+#include <stdbool.h>			//for boolean var
 /*
 
 int connect(int sockfd, const struct sockaddr *addr,
@@ -16,16 +16,19 @@ int connect(int sockfd, const struct sockaddr *addr,
 #define PORT 8080
 #define BUFFSIZE 1024
 
+bool server;
+
 void *read_client(void* client_socket)
 {
 	int client = *(int*)client_socket;
 	char buffer[BUFFSIZE];
 
-	while(1)
+	while(server)
 	{
 		memset(&buffer, 0, sizeof(buffer));
 		read(client, buffer, sizeof(buffer));
-		printf("%s\n",buffer);
+		if(strcmp(buffer, "q")==0) server=false;
+		else printf("Server: %s\n",buffer);
 	}
 
 	return NULL;
@@ -36,12 +39,13 @@ void *write_client(void* client_socket)
 	int client = *(int*)client_socket;
 	char buffer[BUFFSIZE];
 
-	while(1)
+	while(server)
 	{
 		memset(&buffer, 0, sizeof(buffer));
 		fgets(buffer,sizeof(buffer),stdin);
 		buffer[strlen(buffer)-1]='\0';
 		write(client, buffer, sizeof(buffer));
+		if(strcmp(buffer, "q")) server=false;
 	}
 
 	return NULL;
@@ -73,6 +77,7 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	server = true;
 	pthread_t read, write;
 
 	pthread_create(&read, NULL, read_client, (void*)&client_socket);
@@ -83,43 +88,3 @@ int main()
 
 	return 0;
 }
-
-/*
-
-int main()
-{
-	struct sockaddr_in address;
-	int sockfd = 0,valread,ret;
-	struct sockaddr_in serv_addr;
-	char *hello = "Hello from client";
-	char buffer[1024] = {0};
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd <0)
-	{
-		perror("socker creation failed");
-		exit(EXIT_FAILURE);
-	}
-
-	memset(&serv_addr, '0', sizeof(serv_addr));
-
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-	ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
-	if(ret<0)
-	{
-		perror("Connection error");
-		exit(EXIT_FAILURE);
-	}
-	send(sockfd, hello, strlen(hello), 0);
-	printf("Hello messsage sent\n");
-	valread = read(sockfd, buffer, 1024);
-	printf("%s\n",buffer);
-
-	return 0;
-
-}
-*/
--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <string.h>				//strlen()
 #include <netinet/in.h>			//for struct sockaddr
 #include <pthread.h>			//for multi threading
+#include <stdbool.h>			//for boolean var
 /*
 
 int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,47 +28,44 @@ int new_socket = accept(int sockfd, struct sockaddr *addr,
 #define BUFFSIZE 1024
 #define MAX_CLIENTS 5
 
+int n_conn;
+int fd[MAX_CLIENTS];
+bool server;
+
 void *read_client(void* client_socket)
 {
 	int client = *(int*)client_socket;
 	char buffer[BUFFSIZE];
 
-	while(1)
+	while(server)
 	{
 		memset(&buffer, 0, sizeof(buffer));
 		read(client, buffer, sizeof(buffer));
-		printf("%s\n",buffer);
+		if(strcmp(buffer,"q")==0) break;
+		else printf("Client(%d): %s\n",client,buffer);
 	}
+	printf("client %d left\n", client);
 
 	return NULL;
 }
 
-void *write_client(void* client_socket)
+void *write_client()
 {
-	int client = *(int*)client_socket;
+	//int client = *(int*)client_socket;
 	char buffer[BUFFSIZE];
 
-	while(1)
+	while(server)
 	{
 		memset(&buffer, 0, sizeof(buffer));
 		fgets(buffer,sizeof(buffer),stdin);
 		buffer[strlen(buffer)-1]='\0';
-		write(client, buffer, sizeof(buffer));
+
+		if(strcmp(buffer, "q")==0)
+			server=false;
+
+		for(int j=0;j<n_conn;j++)
+			write(fd[j], buffer, sizeof(buffer));
 	}
-
-	return NULL;
-}
-
-void *client_handler(void* client_socket)
-{
-	int client = *(int*)client_socket;
-	printf("Accepted client %d\n",client);
-	pthread_t read, write;
-	pthread_create(&read, NULL, read_client, (void*)client_socket);
-	pthread_create(&write, NULL, write_client, (void*)client_socket);
-
-	pthread_join(read, NULL);
-	pthread_join(write, NULL);
 
 	return NULL;
 }
@@ -105,105 +103,28 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	pthread_t write;
+	pthread_create(&write, NULL, write_client, NULL);
+	server=true;
 
-	for(int i=0;i<MAX_CLIENTS;i++)
+	for(n_conn=0;n_conn<MAX_CLIENTS;n_conn++)
 	{
 		client_socket = accept(server_socket, 
 							(struct sockaddr *)&server_addr,
 									(socklen_t*)&addrlen);
 
-		pthread_create(&clients[i], NULL, client_handler, 
+		pthread_create(&clients[n_conn], NULL, 
+									read_client, 
 									(void*)&client_socket);
+
+		fd[n_conn] = client_socket;
 	}
 
-	for(int i=0;i<MAX_CLIENTS;i++)
-		pthread_join(clients[i], NULL);
+	for(int j=0;j<MAX_CLIENTS;j++)
+		pthread_join(clients[j], NULL);
+
+	pthread_join(write, NULL);
 
 
 	return 0;
 }
-
-/*
-
-int main()
-{
-	int server_fd, new_socket,new_socket2, valread;
-	int ret;
-	struct sockaddr_in address;
-	int opt = 0;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char *hello = "Hello from server";
-
-	//creating a socker file descriptor
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-	if(server_fd == 0)
-	{
-		perror("socker failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	//Forcefully attaching socker to the port 8080
-	//ret = setsockopt(server_fd, SOL_SOCKET, 
-							//SO_REUSEADDR | SO_REUSEPORT,
-							//	&opt, sizeof(opt));
-	if(ret)
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-
-	//forcefully binding the socket
-	ret = bind(server_fd, (struct sockaddr *)&address,
-						sizeof(address));
-
-	if(ret<0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-
-	printf("Before listen\n");
-	ret = listen(server_fd, NUMCONN);
-	printf("After listen\n");
-	if(ret<0)
-	{
-		perror("listen failed");
-		exit(EXIT_FAILURE);
-	}
-
-	new_socket = accept(server_fd, (struct sockaddr *)&address,
-										(socklen_t*)&addrlen);
-
-	new_socket2 = accept(server_fd, (struct sockaddr *)&address,
-										(socklen_t*)&addrlen);
-
-	if(new_socket<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-
-	if(new_socket2<0)
-	{
-		perror("accept2");
-		exit(EXIT_FAILURE);
-	}
-	valread = read(new_socket, buffer, 1024);
-	printf("%s\n",buffer);
-	valread = read(new_socket2, buffer, 1024);
-	printf("%s\n",buffer);
-	send(new_socket, hello , strlen(hello), 0);
-	send(new_socket, hello , strlen(hello), 0);
-	printf("Hello messages sent\n");
-
-	return 0;
-
-}
-
-*/
